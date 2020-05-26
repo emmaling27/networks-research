cb4ffb9fe9e55293d4f4548f04016a33a890ea82
Emma Ling <emmaling27@gmail.com>
2020-05-26 10:39:46 -0600

    refactored with count object

--- sbm.py
+++ sbm.py
@@ -1,5 +1,18 @@
 import networkx as nx
 from scipy.special import comb
+import attr
+
+@attr.s
+class Count(object):
+    n = attr.ib()
+    monochromatic = attr.ib(default=0)
+    bichromatic = attr.ib(default=0)
+
+    def count_edge(self, u, v):
+        if (u < self.n / 2) != (v < self.n / 2):
+            self.bichromatic += 1
+        else:
+            self.monochromatic += 1
 
 class SBM():
 
@@ -14,6 +27,7 @@ class SBM():
 
     def is_bichromatic(self, u, v):
         return (u < self.n / 2) != (v < self.n / 2)
+    
 
     def get_bichromatic_fraction(self):
         bichromatic = 0
@@ -36,16 +50,13 @@ class SBM():
         return monochromatic, bichromatic
     
     def _count_possible_edges(self, local_bridge):
-        monochromatic, bichromatic = 0, 0
+        count = Count(self.n)
         for u in range(self.n):
             for v in range(u+1, self.n):
                 if not self.g.has_edge(u, v) and \
                      (self.is_local_bridge(u, v) == local_bridge):
-                    if self.is_bichromatic(u, v):
-                        bichromatic += 1
-                    else:
-                        monochromatic += 1
-        return monochromatic, bichromatic
+                    count.count_edge(u, v)
+        return count
 
     def count_possible_local_bridges(self):
         return self._count_possible_edges(local_bridge=True)
@@ -54,27 +65,27 @@ class SBM():
         return self._count_possible_edges(local_bridge=False)
 
     def count_wedges(self):
-        monochromatic, bichromatic = 0, 0
+        count = Count(self.n)
         for v in self.g.nodes():
             sorted_neighbors = sorted(self.g.neighbors(v))
             for i in range(len(sorted_neighbors)):
                 for j in range(i + 1, len(sorted_neighbors)):
                     if not self.g.has_edge(sorted_neighbors[i], sorted_neighbors[j]):
-                        if self.is_bichromatic(sorted_neighbors[i], sorted_neighbors[j]):
-                            bichromatic += 1
-                        else:
-                            monochromatic += 1
-        return monochromatic, bichromatic
+                        count.count_edge(sorted_neighbors[i], sorted_neighbors[j])
+        return count
 
-    def predicted_monochromatic_wedges(self):
-        return 3 * 2 * comb(self.n/2, 3) * self.p**2 * (1-self.p) \
-            + self.n * comb(self.n/2, 2) * self.q**2 * (1-self.p)
-    
-    def predicted_bichromatic_wedges(self):
-        return 2 * self.n * comb(self.n/2, 2) * self.p * self.q * (1-self.q)
+    def predicted_wedges(self):
+        return Count(
+            self.n,
+            monochromatic=3 * 2 * comb(self.n/2, 3) * self.p**2 * (1-self.p) \
+            + self.n * comb(self.n/2, 2) * self.q**2 * (1-self.p),
+            bichromatic=2 * self.n * comb(self.n/2, 2) * self.p * self.q * (1-self.q)
+        )
 
-    def predicted_monochromatic_local_bridges(self):
-        return 2 * (1-self.p) * comb(self.n/2, 2) - self.predicted_monochromatic_wedges()
-
-    def predicted_bichromatic_local_bridges(self):
-        return (1-self.q) * (self.n/2) ** 2 - self.predicted_bichromatic_wedges()
+    def predicted_local_bridges(self):
+        wedges = self.predicted_wedges()
+        return Count(
+            self.n,
+            monochromatic=2 * (1-self.p) * comb(self.n/2, 2) - wedges.monochromatic,
+            bichromatic=(1-self.q) * (self.n/2) ** 2 - wedges.bichromatic
+        )

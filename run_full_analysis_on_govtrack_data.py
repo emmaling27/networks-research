1ce733bcfe2b0604ef999a4c6766cef1c700b55a
Emma Ling <emmaling27@gmail.com>
2020-06-15 10:39:53 -0700

    added congress analysis script

---
+++ run_full_analysis_on_govtrack_data.py
@@ -0,0 +1,12 @@
+from congress_data_cleaner import CongressDataCleaner
+from grapher import Grapher
+from plotter import Plotter
+
+congress_data_cleaner = CongressDataCleaner('data/govtrack_cosponsor_data/raw', 'data/govtrack_cosponsor_data/clean/', 'data/party_affiliation/')
+congress_data_cleaner.clean_all_data()
+
+grapher = Grapher('data/govtrack_cosponsor_data/clean/full/', 'analysis/govtrack_cosponsor_data')
+grapher.get_all_counts()
+
+plotter = Plotter('analysis/govtrack_cosponsor_data/counts', 'plots/govtrack_cosponsor_data', 100, 20)
+plotter.plot_all()

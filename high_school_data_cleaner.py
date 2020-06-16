322929a7a464cc30ce2c10433bda7dc6274604f7
Emma Ling <emmaling27@gmail.com>
2020-06-16 11:34:06 -0700

    high school data cleaner

---
+++ high_school_data_cleaner.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import os
+from os.path import join
+
+class HighSchoolDataCleaner():
+
+    def __init__(self, raw_path, clean_path):
+        self.raw_path = raw_path
+        self.clean_path = clean_path
+
+    def clean_df(self, hs_df, type_df):
+        """Returns cleaned df in standard format"""
+        hs_df.columns = ['time', 'u', 'v', 'class1', 'c2']
+        type_df.columns = ['id', 'class', 'gender']
+        type_df = type_df.set_index('id')
+        df = hs_df.join(type_df, on='u')
+        df.columns = ['time', 'u', 'v', 'c', 'c1', 'c2', 'u_type']
+        df = df.join(type_df, on='v')
+        df.columns = ['time', 'u', 'v', 'c', 'c1', 'c2', 'u_type', 'c2', 'v_type']
+        df = df.drop(['c', 'c1', 'c2', 'c2'], axis=1)
+        return df
+
+    def clean_all_data(self, t=None):
+        """Creates and saves bill dfs of all congresses, optionally below threshold t"""
+        for year in range(2011, 2013):
+            hs_df = pd.read_csv(join(self.raw_path, 'highschool_' + str(year) + '.tsv'), sep='\t')
+            type_df = pd.read_csv(join(self.raw_path, 'metadata_' + str(year) + '.tsv'), sep='\t')
+            self.clean_df(hs_df, type_df).to_csv(join(self.clean_path, 'highschool_' + str(year) + '.csv'), index=False)
